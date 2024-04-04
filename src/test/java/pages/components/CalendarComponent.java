7afa8893340e565e459750449893cbadb54a636b
Egor Melnichyk <egor.mel8@gmail.com>
2024-04-05 00:34:46 +0300

    Refactor 2.0 +

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -7,7 +7,7 @@ public class CalendarComponent {
     public void setDateOfBirth(String day, String month, String year) {
         $("select.react-datepicker__month-select").selectOptionContainingText(month);
         $("select.react-datepicker__year-select").selectOptionByValue(year);
-        if (Integer.valueOf(day) < 10) {
+        if (10 > Integer.parseInt(day)) {
             $(".react-datepicker__day--00" + day
                     + ":not(.react-datepicker__day--outside-month)").click();
         } else {
