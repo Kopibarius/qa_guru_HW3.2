c03a13bec675c64ce009c8400bbd0b980b424f41
Egor Melnichyk <egor.mel8@gmail.com>
2024-06-05 12:17:03 +0300

    Added attach screen and video

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,12 +1,14 @@
-import Utils.RandomDateUtils;
+import Attachments.Attach;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationPage;
 
-import static com.codeborne.selenide.Selenide.sleep;
+import java.util.Map;
 
 public class TestBase {
     public String[] genders = {"Male", "Female", "Other"};
@@ -20,5 +22,27 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("allure", new AllureSelenide());
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
     }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
+
+
 }
